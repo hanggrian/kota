91151a62c443beba415bee64902caedab23f4690
Hendra Anggrian <hendraanggrian@gmail.com>
2017-08-03 02:59:34 -0700

    nullable ViewGroup

--- kota/src/com/hendraanggrian/kota/view/LayoutInflaters.kt
+++ kota/src/com/hendraanggrian/kota/view/LayoutInflaters.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import org.xmlpull.v1.XmlPullParser
 
 @JvmOverloads
-fun Context.inflateBy(@LayoutRes resource: Int, root: ViewGroup, attachToRoot: Boolean = false) = LayoutInflater.from(this).inflate(resource, root, attachToRoot)!!
+fun Context.inflateBy(@LayoutRes resource: Int, root: ViewGroup?, attachToRoot: Boolean = false) = LayoutInflater.from(this).inflate(resource, root, attachToRoot)!!
 
 @JvmOverloads
-fun Context.inflateBy(parser: XmlPullParser, root: ViewGroup, attachToRoot: Boolean = false) = LayoutInflater.from(this).inflate(parser, root, attachToRoot)!!
+fun Context.inflateBy(parser: XmlPullParser, root: ViewGroup?, attachToRoot: Boolean = false) = LayoutInflater.from(this).inflate(parser, root, attachToRoot)!!
