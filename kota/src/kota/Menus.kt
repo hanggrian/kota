2320cd415e094f53370c0a8ddc6420ee10d5c6b6
Hendra Anggrian <hendraanggrian@gmail.com>
2017-10-20 22:44:08 +0700

    operator fun

--- kota/src/kota/Menus.kt
+++ kota/src/kota/Menus.kt
@@ -5,18 +5,21 @@ package kota
 import android.view.Menu
 import android.view.MenuItem
 
-inline val Menu.items: List<MenuItem> get() = (0 until size()).map { getItem(it) }
+inline fun Menu.find(id: Int): MenuItem = findItem(id)!!
 
+inline fun Menu.findNullable(id: Int): MenuItem = findItem(id)
+
+inline operator fun Menu.get(index: Int): MenuItem = getItem(index)!!
+inline fun Menu.getOrNull(index: Int): MenuItem? = if (index in 0..lastIndex) getItem(index) else null
+
+inline val Menu.items: List<MenuItem> get() = (0 until size()).map { getItem(it) }
 inline val Menu.firstItem: MenuItem get() = getItem(0)
 inline val Menu.firstItemOrNull: MenuItem? get() = if (isEmpty) null else firstItem
-
 inline val Menu.lastItem: MenuItem get() = getItem(lastIndex)
 inline val Menu.lastItemOrNull: MenuItem? get() = if (isEmpty) null else lastItem
 
 inline val Menu.isEmpty: Boolean get() = size() == 0
 inline val Menu.lastIndex: Int get() = size() - 1
-
-inline fun Menu.getItemOrNull(index: Int): MenuItem? = if (index in 0..lastIndex) getItem(index) else null
 
 inline fun Menu.forEachItem(action: (MenuItem) -> Unit) {
     for (item in items) action(item)
--- kota/src/kota/Views.kt
+++ kota/src/kota/Views.kt
@@ -9,8 +9,6 @@ import android.support.annotation.IdRes
 import android.view.View
 import android.view.ViewGroup
 
-inline val Activity.contentView: View? get() = findViewById<ViewGroup>(android.R.id.content)?.getChildAt(0)
-
 inline fun <reified T : View> View.find(@IdRes id: Int): T = findViewById(id)!!
 inline fun <reified T : View> Activity.find(@IdRes id: Int): T = findViewById(id)!!
 inline fun <reified T : View> Fragment.find(@IdRes id: Int): T = view?.findViewById(id)!!
@@ -20,6 +18,29 @@ inline fun <reified T : View> View.findNullable(@IdRes id: Int): T? = findViewBy
 inline fun <reified T : View> Activity.findNullable(@IdRes id: Int): T? = findViewById(id)
 inline fun <reified T : View> Fragment.findNullable(@IdRes id: Int): T? = view?.findViewById(id)
 inline fun <reified T : View> Dialog.findNullable(@IdRes id: Int): T? = findViewById(id)
+
+inline operator fun ViewGroup.get(index: Int): View = getChildAt(index)!!
+inline fun ViewGroup.getOrNull(index: Int): View? = getChildAt(index)
+
+inline val ViewGroup.childs: List<View> get() = (0 until childCount).map { getChildAt(it) }
+inline val ViewGroup.firstChild: View get() = getChildAt(0)
+inline val ViewGroup.firstChildOrNull: View? get() = if (isEmpty) null else firstChild
+inline val ViewGroup.lastChild: View get() = getChildAt(lastIndex)
+inline val ViewGroup.lastChildOrNull: View? get() = if (isEmpty) null else lastChild
+
+inline val ViewGroup.isEmpty: Boolean get() = childCount == 0
+inline val ViewGroup.lastIndex: Int get() = childCount - 1
+
+inline fun ViewGroup.forEachChild(action: (View) -> Unit) {
+    for (child in childs) action(child)
+}
+
+inline fun ViewGroup.forEachChildIndexed(action: (Int, View) -> Unit) {
+    var i = 0
+    for (child in childs) action(i++, child)
+}
+
+inline val Activity.contentView: View? get() = findViewById<ViewGroup>(android.R.id.content)?.getChildAt(0)
 
 inline fun <V : View> V.setVisibilityThen(
         visibility: Int,
@@ -49,23 +70,3 @@ inline fun <V : View> V.setVisibleThen(
         visible: Boolean,
         block: V.() -> Unit
 ) = setVisibilityThen(if (visible) View.VISIBLE else View.GONE, block)
-
-inline val ViewGroup.childs: List<View> get() = (0 until childCount).map { getChildAt(it) }
-
-inline val ViewGroup.firstChild: View get() = getChildAt(0)
-inline val ViewGroup.firstChildOrNull: View? get() = if (isEmpty) null else firstChild
-
-inline val ViewGroup.lastChild: View get() = getChildAt(lastIndex)
-inline val ViewGroup.lastChildOrNull: View? get() = if (isEmpty) null else lastChild
-
-inline val ViewGroup.isEmpty: Boolean get() = childCount == 0
-inline val ViewGroup.lastIndex: Int get() = childCount - 1
-
-inline fun ViewGroup.forEachChild(action: (View) -> Unit) {
-    for (child in childs) action(child)
-}
-
-inline fun ViewGroup.forEachChildIndexed(action: (Int, View) -> Unit) {
-    var i = 0
-    for (child in childs) action(i++, child)
-}
