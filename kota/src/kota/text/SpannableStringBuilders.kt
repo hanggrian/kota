5e0c8dd9fb1389ef1b6984953fb28533251b9da2
Hendra Anggrian <hendraanggrian@gmail.com>
2017-12-03 06:03:51 +0700

    added append line

--- kota/src/kota/text/SpannableStringBuilders.kt
+++ kota/src/kota/text/SpannableStringBuilders.kt
@@ -11,6 +11,8 @@ import android.text.Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
 /** Returns a new [SpannableStringBuilder] from source or the source itself if it is already an instance of [SpannableStringBuilder]. */
 inline fun CharSequence.toSpannableBuilder(): SpannableStringBuilder = valueOf(this)
 
+inline fun SpannableStringBuilder.appendln() = append("\n")
+
 /** Append text with [spans] applied. */
 @JvmOverloads
 inline fun SpannableStringBuilder.append(
@@ -24,3 +26,11 @@ inline fun SpannableStringBuilder.append(
     spanRange(start, end, *spans, flags = flags)
     return this
 }
+
+/** Append text with [spans] applied with line separator. */
+@JvmOverloads
+inline fun SpannableStringBuilder.appendln(
+        text: CharSequence,
+        vararg spans: (s: String) -> Any,
+        flags: Int = SPAN_EXCLUSIVE_EXCLUSIVE
+): SpannableStringBuilder = append(text, *spans, flags = flags).appendln()
